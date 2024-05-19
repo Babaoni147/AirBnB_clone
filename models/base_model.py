d3c27e9e801e09e2b439df2abcc5fa97c53c86c7
Babaoni147 <obahedwin@ymail.com>
2024-05-19 06:01:07 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-"""
-BaseModel class that defines all common attributes/methods for other classes
-"""
+"""BaseModel class defines common attributes/methods"""
 
 import uuid
 from datetime import datetime
 import models
 import json
-isoform_time = "%Y-%m-%dT%H:%M:%S.%f"
+
+time_form = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """ Definning the base class from which the other
-        classes will inherit
-    """
+    """Define HBnB Base_Model"""
     def __init__(self, *args, **kwargs):
-        """ Initialization of the object/instance attributes """
-        if kwargs is None or len(kwargs) == 0:
+        """Initialization of the object/instance attributes"""
+
+        if len(kwargs) != 0;
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, time_form)
+                elif:
+                    self.__dict__[key] = val
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    self.id = value
-                elif key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, isoform_time)
-                elif key is "__class__":
-                    pass
-                elif key is not "__class__":
-                    self.__dict__[key] = value
+            model.storage.new(self)
 
     def __str__(self):
         """ Writing the __str__ method """
@@ -38,10 +32,7 @@ class BaseModel:
                                      self.id, self.__dict__)
 
     def save(self):
-        """ Public instance methods:
-            updates the public instance attribute updated_at
-            with the current datetime
-        """
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
         models.storage.save()
 
@@ -50,7 +41,7 @@ class BaseModel:
             returns a dictionary containing all keys/values
             of __dict__.
         """
-        dic_BaseClass = self.__dict__.copy()
+        dic_Baseclass = self.__dict__.copy()
         dic_BaseClass["__class__"] = self.__class__.__name__
         dic_BaseClass["created_at"] = self.created_at.isoformat()
         dic_BaseClass["updated_at"] = self.updated_at.isoformat()
