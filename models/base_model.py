5972180082177a465a9e862c5168282fd3a26098
Babaoni147 <obahedwin@ymail.com>
2024-05-19 06:36:32 +0100

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 import uuid
 from datetime import datetime
 import models
-import json
 
 time_form = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -12,27 +11,35 @@ time_form = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
     """Define HBnB Base_Model"""
     def __init__(self, *args, **kwargs):
-        """Initialization of the object/instance attributes"""
+        """ Initialization of the object/instance attributes """
 
-        if len(kwargs) != 0;
-            for key, val in kwargs.items():
+        if kwargs:
+            for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(val, time_form)
-                elif:
-                    self.__dict__[key] = val
+                    setattr(self, key, datetime.strptime(value, time_form))
+                elif key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            model.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """ Writing the __str__ method """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """ Public instance methods:
+            updates the public instance attribute updated_at
+            with the current datetime
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
@@ -41,7 +48,7 @@ class BaseModel:
             returns a dictionary containing all keys/values
             of __dict__.
         """
-        dic_Baseclass = self.__dict__.copy()
+        dic_BaseClass = self.__dict__.copy()
         dic_BaseClass["__class__"] = self.__class__.__name__
         dic_BaseClass["created_at"] = self.created_at.isoformat()
         dic_BaseClass["updated_at"] = self.updated_at.isoformat()
